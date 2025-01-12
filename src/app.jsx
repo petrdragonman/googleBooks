e42c331121f8938613319ed4f9a0ca997eefea39
petrdragonman <petrdragonman@me.com>
2025-01-13 08:29:51 +1100

    setup

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import './app.scss'
+
+export function App() {
+
+  return (
+    <>
+      <h2>Welcome</h2>
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import { render } from 'preact'
+import './index.scss'
+import { App } from './app.jsx'
+
+render(<App />, document.getElementById('app'))
