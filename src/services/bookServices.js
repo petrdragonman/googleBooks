ba989e8ef63daf37929950c660d501996b8d9b8f
petrdragonman <petrdragonman@me.com>
2025-01-18 08:11:34 +1100

    adds search capabilities

--- src/services/bookServices.js
+++ src/services/bookServices.js
@@ -3,13 +3,13 @@ import cleanUpData from "./cleanUpData";
 export const getBooks = async ({searchTerm}) => {
     // GET https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=yourAPIKey
     const APIKey = 'AIzaSyBPrRwFeAPbma8oXMoeFzQS0-wS26mBTnw';
-    // &key=yourAPIKey
     const baseUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
-    console.log("in get books: ", searchTerm);
+    //console.log("in get books: ", search );
+    console.log(typeof searchTerm)
     //const url = 'https://www.googleapis.com/books/v1/volumes?q=cat';
     
-    const url = `${baseUrl}${searchTerm}&key=${APIKey}`;
-    //const url = baseUrl+"cats&key="+APIKey;
+    //const url = `${baseUrl}${searchTerm}&key=${APIKey}`;
+    const url = baseUrl+searchTerm+"&key="+APIKey;
     console.log("url: ", url);
 
     const response = await fetch(url);
