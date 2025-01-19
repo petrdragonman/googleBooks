cf8f7cdc4a2015c4534f03f07ddb936334c83d2b
petrdragonman <petrdragonman@me.com>
2025-01-19 12:46:38 +1100

    adds responsive design

--- src/components/BookDetailsModal/BookDetailsModal.jsx
+++ src/components/BookDetailsModal/BookDetailsModal.jsx
@@ -7,20 +7,23 @@ const BookDetailsModal = ({ setIsModal, book }) => {
   };
   return (
     <article className={classes.wrapper}>
-      <section className={classes.card}>
-        <span className={classes.close} onClick={handleClick}>&times;</span>
-        <span className={classes.title}>{book.title}</span>
-        <section className={classes.authors}>
-          {book.authors.map((author) => (
-            <span className={classes.author} key={author}>
-              {author}
-            </span>
-          ))}
+      <section className={classes.modal}>
+        <section className={classes.card}>
+          <span className={classes.close} onClick={handleClick}>&times;</span>
+          <span className={classes.title}>{book.title}</span>
+          <section className={classes.authors}>
+            {book.authors.map((author) => (
+              <span className={classes.author} key={author}>
+                {author}
+              </span>
+            ))}
+          </section>
+          {book.image && <img src={book.image.thumbnail} alt="book image" />}
+          <span className={classes.description}>{book.description}</span>
+          <Button onClick={handleClick}>CLOSE</Button>
         </section>
-        {book.image && <img src={book.image.thumbnail} alt="book image" />}
-        <span className={classes.description}>{book.description}</span>
-        <Button onClick={handleClick}>CLOSE</Button>
       </section>
+      
     </article>
   );
 };
