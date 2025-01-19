02b7a2840832a8a058cf2b48a683f0f05a6b1dd0
petrdragonman <petrdragonman@me.com>
2025-01-19 22:37:03 +1100

    adds publisher, published date and country to the modal

--- src/services/cleanUpData.js
+++ src/services/cleanUpData.js
@@ -1,17 +1,27 @@
 const cleanUpData = (data) => {
+  console.log(data);
   const result = data.items.map((item) => {
     const id = item.id;
     const title = item.volumeInfo.title;
     const description = item.volumeInfo.description;
     const authors = item.volumeInfo.authors;
     const image = item.volumeInfo.imageLinks;
-    
+    const publisher = item.volumeInfo.publisher;
+    const publishedDate = item.volumeInfo.publishedDate;
+    const language = item.volumeInfo.language;
+    const pageCount = item.volumeInfo.pageCount;
+    const country = item.saleInfo.country;
     return {
       id,  
       title,
       description,
       authors,
       image,
+      publisher,
+      publishedDate,
+      language,
+      pageCount,
+      country,
     };
   });
   return result;
@@ -21,6 +31,9 @@ export default cleanUpData;
 
 
 /**
+ * When a user clicks a book in the grid, a modal should appear with more book information, 
+ * think about release, publish date, country, languages, etc.
+
  * const cleanUpData = (data) => {
   const result = data.items.map((item) => {
     const id = item.id;
