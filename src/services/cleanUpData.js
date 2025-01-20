8e13770855f4e86519c4a7377426272f194264cf
petrdragonman <petrdragonman@me.com>
2025-01-20 13:22:07 +1100

    cleans up secret

--- src/services/cleanUpData.js
+++ src/services/cleanUpData.js
@@ -1,5 +1,4 @@
 const cleanUpData = (data) => {
-  console.log(data);
   const result = data.items.map((item) => {
     const id = item.id;
     const title = item.volumeInfo.title;
@@ -28,29 +27,3 @@ const cleanUpData = (data) => {
 };
 
 export default cleanUpData;
-
-
-/**
- * When a user clicks a book in the grid, a modal should appear with more book information, 
- * think about release, publish date, country, languages, etc.
-
- * const cleanUpData = (data) => {
-  const result = data.items.map((item) => {
-    const id = item.id;
-    const title = item.volumeInfo.title;
-    const description = item.volumeInfo.description;
-    const authors = item.volumeInfo.authors;
-    const image = item.volumeInfo.imageLinks;
-    
-    return {
-      id,  
-      title,
-      description,
-      authors,
-      image,
-    };
-  });
-  console.log("clean up data result: ", result);
-  return result;
-};
- */
